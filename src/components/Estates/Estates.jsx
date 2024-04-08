30877f57e9456433f86d7920f7acd81cbed182b9
Asad9340 <asadulimran1999@gmail.com>
2024-04-08 17:28:58 +0600

    added estate section

---
+++ src/components/Estates/Estates.jsx
@@ -0,0 +1,39 @@
+import { Button } from '@material-tailwind/react';
+import { FaLocationDot } from 'react-icons/fa6';
+import { HiCurrencyDollar } from 'react-icons/hi2';
+
+function Estates({ estate }) {
+  console.log(estate);
+  const { image, title, name, location, price, area, status } = estate;
+  return (
+    <div className="relative mb-10">
+      <img className="rounded-lg z-10 h-[400px] w-full" src={image} alt="" />
+      <div className="-mt-4 hover:absolute hover:-mt-20 duration-300 bg-secondary text-textSecondary hover:bg-red-500 z-40 w-full px-4 md:px-6 py-6 md:py-8 rounded-b-lg">
+        <h2 className="text-xl md:text-2xl font-medium md:font-semibold text-center">
+          {title}
+        </h2>{' '}
+        <hr className="mx-10 bg-black" />
+        <p>Name: {name}</p>
+        <div className="flex justify-between pr-6 ">
+          <p className="flex gap-1 items-center">
+            <FaLocationDot />
+            Location:
+            {location}
+          </p>
+          <p>Status:{status}</p>
+        </div>
+        <div className="flex justify-between pr-6 ">
+          <p className="flex gap-1 items-center">
+            <HiCurrencyDollar /> Price: {price}
+          </p>
+          <p>Area: {area}</p>
+        </div>
+        <div className='text-center mt-4'>
+          <Button color="indigo">View Property</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Estates;
--- src/components/Luxury/Luxury.jsx
+++ src/components/Luxury/Luxury.jsx
@@ -1,4 +1,7 @@
+import Estates from '../Estates/Estates';
+import useEstateData from './../../Hooks/useEstateData';
 function Luxury() {
+  const { data } = useEstateData();
   return (
     <div className="my-4 md:my-8 lg:my-10">
       <div className="border p-4 bg-primary rounded-md">
@@ -6,8 +9,10 @@ function Luxury() {
           Luxury
         </h2>
       </div>
-      <div>
-        
+      <div className="grid gap-4 md:gap-8 grid-cols-1 md:grid-cols-2">
+        {data.map(estate => (
+          <Estates key={estate.id} estate={estate} />
+        ))}
       </div>
     </div>
   );
