beac2df43aad85181c13ed034270666524829a13
Asad9340 <asadulimran1999@gmail.com>
2024-04-14 00:29:59 +0600

    change in about page

--- src/components/AboutPage/About.jsx
+++ src/components/AboutPage/About.jsx
@@ -11,25 +11,25 @@ const About = () => {
         <h1 className="text-3xl font-bold mb-4 text-center animate__animated animate__bounce">
           About Us
         </h1>
-        <p className="text-lg text-gray-700 mb-6">
+        <p className="text-lg text-gray-700 mb-6 text-center">
           Welcome to BookBud, your ultimate destination for discovering and
           exploring a wide range of books from various genres. Whether you are a
           passionate reader or just starting your reading journey, BookBud is
           here to help you find your next favorite book.
         </p>
-        <p className="text-lg text-gray-700 mb-6">
+        <p className="text-lg text-gray-700 mb-6 text-center">
           Our mission is to make the process of discovering and acquiring books
           as enjoyable and seamless as possible. We strive to provide a
           user-friendly platform where book lovers can connect, share
           recommendations, and enhance their reading experience.
         </p>
-        <p className="text-lg text-gray-700 mb-6">
+        <p className="text-lg text-gray-700 mb-6 text-center">
           At BookBud, we believe in the power of storytelling and the
           transformative impact of literature. That is why we are committed to
           curating an extensive collection of books that cater to diverse
           interests and preferences.
         </p>
-        <p className="text-lg text-gray-700 mb-6">
+        <p className="text-lg text-gray-700 mb-6 text-center">
           Thank you for choosing BookBud as your literary companion. Happy
           reading!
         </p>
